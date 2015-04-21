2353f08a2cea64e3a0f0ff4e57f0eea971bc8bb9
mellzamora <mellzamora@outlook.com>
2015-04-21 11:37:58 +0800

    refactored deprecated OptionsResolverInterface to OptionsResolver

--- Form/Type/FormatterType.php
+++ Form/Type/FormatterType.php
@@ -13,22 +13,11 @@ namespace Rz\FormatterBundle\Form\Type;
 
 use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\OptionsResolver\Options;
-
 use Sonata\FormatterBundle\Formatter\Pool;
-
-
-
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
-
 use Symfony\Component\Form\AbstractType;
-
-use Ivory\CKEditorBundle\Model\ConfigManagerInterface;
-
-use Sonata\FormatterBundle\Form\EventListener\FormatterListener;
 
 class FormatterType extends AbstractTypeExtension
 {
@@ -49,49 +38,65 @@ class FormatterType extends AbstractTypeExtension
 
     /**
      * {@inheritdoc}
+     *
+     * @todo Remove it when bumping requirements to SF 2.7+
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
 
 
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
         $pool = $this->pool;
         $translator = $this->translator;
 
-        $resolver->setOptional(array('selectpicker_enabled',
-                                   'selectpicker_data_style',
-                                   'selectpicker_title',
-                                   'selectpicker_selected_text_format',
-                                   'selectpicker_show_tick',
-                                   'selectpicker_data_width',
-                                   'selectpicker_data_size',
-                                   'selectpicker_disabled',
-                                   'selectpicker_dropup')
-        );
+        $optionalOptions = array('selectpicker_enabled',
+                'selectpicker_data_style',
+                'selectpicker_title',
+                'selectpicker_selected_text_format',
+                'selectpicker_show_tick',
+                'selectpicker_data_width',
+                'selectpicker_data_size',
+                'selectpicker_disabled',
+                'selectpicker_dropup');
+
+        if (method_exists($resolver, 'setDefined')) {
+            $resolver->setDefined($optionalOptions);
+        } else {
+            // To keep Symfony <2.6 support
+            $resolver->setOptional($optionalOptions);
+        }
 
         $resolver->setDefaults(array(
-                   'inherit_data'      => true,
-                   'event_dispatcher'  => null,
-                   'format_field'      => null,
-                   'format_field_options' => array(
-                       'choices'           => $this->getChoices($pool, $translator)),
-                   'source_field' => null,
-                   'source_field_options'      => array(
-                       'attr' => array('class' => 'span10', 'rows' => 20)
-                   ),
-                   'target_field' => null,
-                   'listener'     => true,
-                    'ckeditor_toolbar_icons'    => array( array(
-                        'Bold', 'Italic', 'Underline',
-                        '-', 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord',
-                        '-', 'Undo', 'Redo',
-                        '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent',
-                        '-', 'Blockquote',
-                        '-', 'Image', 'Link', 'Unlink', 'Table'),
-                        array('Maximize', 'Source')
-                    ),
-                    'ckeditor_basepath'         => 'bundles/sonataformatter/vendor/ckeditor',
-                    'ckeditor_context'          => null,
-                   'selectpicker_enabled' => false,
+            'inherit_data'      => true,
+            'event_dispatcher'  => null,
+            'format_field'      => null,
+            'format_field_options' => array(
+                'choices'           => $this->getChoices($pool, $translator)),
+            'source_field' => null,
+            'source_field_options'      => array(
+                'attr' => array('class' => 'span10', 'rows' => 20)
+            ),
+            'target_field' => null,
+            'listener'     => true,
+            'ckeditor_toolbar_icons'    => array( array(
+                'Bold', 'Italic', 'Underline',
+                '-', 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord',
+                '-', 'Undo', 'Redo',
+                '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent',
+                '-', 'Blockquote',
+                '-', 'Image', 'Link', 'Unlink', 'Table'),
+                array('Maximize', 'Source')
+            ),
+            'ckeditor_basepath'         => 'bundles/sonataformatter/vendor/ckeditor',
+            'ckeditor_context'          => null,
+            'selectpicker_enabled' => false,
         ));
     }
 
